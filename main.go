6779ce961f33faa5f4a9b0fd7f408415ceba5022
Yann Malet <yann.malet@gmail.com>
2014-07-17 16:01:55 +0200

    Adding a plugin manager and 2 simple plugins in addition of the one that store in ES

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
+	"os"
+	"os/signal"
+	"strings"
+	"sync"
+	"syscall"
 
+	"github.com/BotBotMe/botbot-bot/common"
+	"github.com/BotBotMe/botbot-bot/line"
 	"github.com/golang/glog"
 )
 
@@ -12,10 +20,83 @@ const (
 	esType  = "line"
 )
 
+func listen(wg sync.WaitGroup, queue common.Queue, quit chan struct{}) chan line.Line {
+
+	lines := make(chan line.Line)
+	go func(lines chan line.Line, wg sync.WaitGroup) {
+		wg.Add(1)
+		for {
+			select {
+			case <-quit:
+				wg.Done()
+				glog.V(2).Infoln("Quiting the listen goroutine because we received a quit chan")
+				return
+			default:
+				// Blocking on lpop for 5 s
+				_, val, err := queue.Blpop([]string{"q"}, 5)
+				if err != nil {
+					glog.Errorln("An error occured while blpop on 'q'", err)
+				}
+				var l line.Line
+				err = json.Unmarshal(val, &l)
+				if err != nil && err.Error() == "unexpected end of JSON input" {
+					glog.V(3).Infoln("blpop timeout", err)
+				} else if err != nil {
+					glog.Errorln("An error occured while unmarshalling the line")
+				} else {
+					glog.V(2).Infoln("line", l)
+					lines <- l
+				}
+			}
+		}
+	}(lines, wg)
+	return lines
+}
+
+func EsStore(l line.Line) {
+	es := NewElasticsearch(esAddr, esIndex, esType)
+	es.Index(&l)
+}
+
+func Ping(l line.Line) {
+	if strings.Contains(l.Content, "ping") {
+		glog.Infoln("Pong")
+	}
+}
+
+func Debug(l line.Line) {
+	glog.V(3).Infoln("[Debug] line.Command", l.Command, "line.Content", l.Content)
+}
+
 func main() {
+	var wg sync.WaitGroup
 	flag.Parse() // We need to capture arguments for glog
 	glog.Infoln("Starting botbot-elasticsearch pointing to", esAddr)
-	line := struct{ Text, Nick string }{Text: "hello world", Nick: "yml"}
-	es := NewElasticsearch(esAddr, esIndex, esType)
-	es.Index(&line)
+
+	// Channels declaration
+	sigs := make(chan os.Signal, 1)
+	quit := make(chan struct{})
+
+	// listen for syscall to cleanly terminate goroutine
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	plugins := []func(line.Line){EsStore, Ping, Debug}
+	queue := common.NewRedisQueue()
+
+	lines := listen(wg, queue, quit)
+	condition := true
+	for condition {
+		select {
+		case l := <-lines:
+			for _, p := range plugins {
+				p(l)
+			}
+		case <-sigs:
+			glog.Infoln("Closting botbot-elasticsearch")
+			close(quit)
+			wg.Wait()
+			condition = false
+			break
+		}
+	}
 }
