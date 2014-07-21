6dfec168c1acf57cb063b29b2edb4ee23fcf687c
Yann Malet <yann.malet@gmail.com>
2014-07-21 15:53:58 +0200

    Modify the interface to isolate the plugins a bit more.

--- elasticsearch.go
+++ elasticsearch.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net/http"
 
 	"github.com/golang/glog"
@@ -30,37 +27,4 @@ func (es *Elasticsearch) newRequest(method string, body io.Reader) (*http.Reques
 	}
 	req.Header.Add("Accept", "application/json")
 	return req, err
-}
-
-func (es *Elasticsearch) Do(method string, v interface{}) (*http.Response, error) {
-	client := &http.Client{}
-	b, err := json.Marshal(v)
-	if err != nil {
-		glog.Errorln("An error occured while marshiling to json:", err)
-		return nil, err
-	}
-	glog.V(3).Infoln("json:", string(b))
-
-	req, err := es.newRequest(method, bytes.NewReader(b))
-	if err != nil {
-		glog.Errorln("An error occured while bulding elasticsearch request:", err)
-		return nil, err
-	}
-	return client.Do(req)
-
-}
-
-func (es *Elasticsearch) Index(v interface{}) (body []byte, err error) {
-	resp, err := es.Do("POST", v)
-	if err != nil {
-		glog.Errorln("An error occured while doing the request to elasticsearch:", err)
-		return nil, err
-	}
-	defer resp.Body.Close()
-	body, err = ioutil.ReadAll(resp.Body)
-	if err != nil {
-		glog.Errorln("An error occured while reading the response from elasticsearch", err)
-	}
-	glog.V(3).Infoln("Response from :", string(body))
-	return body, err
 }
--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"encoding/json"
+	"bytes"
 	"flag"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"os/signal"
 	"strconv"
@@ -21,15 +23,75 @@ const (
 	esType  = "line"
 )
 
+// Context require to Do the PluginResponse
 type Context struct {
 	Storage *Elasticsearch
 	Queue   common.Queue
 }
 
-func listen(wg sync.WaitGroup, ctx *Context, quit chan struct{}) chan line.Line {
+// PluginAction represents an action
+type PluginAction int
 
-	lines := make(chan line.Line)
-	go func(lines chan line.Line, wg sync.WaitGroup) {
+// All this operation
+const (
+	NOOP PluginAction = iota
+	STORE
+	RESPOND
+	REMEMBER
+)
+
+func (a PluginAction) String() string {
+	switch a {
+	case NOOP:
+		return "noop"
+	case STORE:
+		return "Store"
+	case RESPOND:
+		return "Respond"
+	case REMEMBER:
+		return "Remember"
+	default:
+		return "Undefined"
+	}
+}
+
+// Do implements all the PluginAction
+func (a PluginAction) Do(b []byte, ctx *Context) error {
+	switch a {
+	case NOOP:
+		return nil
+	case STORE:
+		client := &http.Client{}
+		req, err := ctx.Storage.newRequest("POST", bytes.NewReader(b))
+		if err != nil {
+			return err
+		}
+		resp, err := client.Do(req)
+		if glog.V(3) {
+			defer resp.Body.Close()
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				return err
+			}
+			glog.Infoln(string(body))
+		}
+		return err
+	case RESPOND:
+		return ctx.Queue.Lpush("bot", b)
+	default:
+		return nil
+	}
+}
+
+// PluginResponse is the type returned by a Plugin
+type PluginResponse struct {
+	Action PluginAction
+	Body   []byte
+}
+
+func listen(wg sync.WaitGroup, ctx *Context, quit chan struct{}) chan []byte {
+	lines := make(chan []byte)
+	go func(lines chan []byte, wg sync.WaitGroup) {
 		wg.Add(1)
 		for {
 			select {
@@ -40,18 +102,13 @@ func listen(wg sync.WaitGroup, ctx *Context, quit chan struct{}) chan line.Line 
 			default:
 				// Blocking on lpop for 5 s
 				_, val, err := ctx.Queue.Blpop([]string{"q"}, 5)
-				if err != nil {
-					glog.Errorln("An error occured while blpop on 'q'", err)
-				}
-				var l line.Line
-				err = json.Unmarshal(val, &l)
 				if err != nil && err.Error() == "unexpected end of JSON input" {
 					glog.V(3).Infoln("blpop timeout", err)
 				} else if err != nil {
-					glog.Errorln("An error occured while unmarshalling the line")
-				} else {
-					glog.V(2).Infoln("line", l)
-					lines <- l
+					glog.Errorln("An error occured while blpop on 'q'", err)
+				} else if !bytes.Equal(val, []byte{}) {
+					// Only send val if it is not empty
+					lines <- val
 				}
 			}
 		}
@@ -59,20 +116,82 @@ func listen(wg sync.WaitGroup, ctx *Context, quit chan struct{}) chan line.Line 
 	return lines
 }
 
-func EsStore(l line.Line, ctx *Context) {
-	ctx.Storage.Index(&l)
+// Plugin represents a plugin
+type Plugin struct {
+	Do func([]byte) *PluginResponse
 }
 
-func Ping(l line.Line, ctx *Context) {
-	if strings.EqualFold(l.Content, l.BotNick+": ping") {
-		// TODO the python version does an Lpush
-		ctx.Queue.Lpush("bot",
-			[]byte("WRITE "+strconv.Itoa(l.ChatBotId)+" "+l.Channel+" Are you in need of my services, "+l.User+" ?"))
+// Run starts the plugin and returns the chan you can listen on to get response
+func (p *Plugin) Run(out chan *PluginResponse, quit chan struct{}, wg sync.WaitGroup) chan []byte {
+	in := make(chan []byte)
+	go func(in chan []byte, out chan *PluginResponse, quit chan struct{}, wg sync.WaitGroup) {
+		wg.Add(1)
+		for {
+			select {
+			case b := <-in:
+				out <- p.Do(b)
+			case <-quit:
+				wg.Done()
+				return
+			}
+		}
+
+	}(in, out, quit, wg)
+	return in
+}
+
+// NewPlugin returns a pointer to a plugin
+func NewPlugin(fn func([]byte) *PluginResponse) *Plugin {
+	return &Plugin{Do: fn}
+}
+
+// EsStore is an Elastivsearch plugin func
+func EsStore(b []byte) *PluginResponse {
+	glog.V(3).Infoln("Elastic search plugin", string(b))
+	if bytes.Contains(b, []byte(`,"Command":"PRIVMSG",`)) {
+		return &PluginResponse{Action: STORE, Body: b}
 	}
+	return &PluginResponse{}
 }
 
-func Debug(l line.Line, ctx *Context) {
-	glog.V(3).Infoln("[Debug] line.Command", l.Command, "line.Content", l.Content)
+// Ping is a ping plugin
+func Ping(b []byte) *PluginResponse {
+	glog.V(3).Infoln("Ping plugin", string(b))
+	if b != nil {
+		l, err := line.NewFromJSON(b)
+		if err != nil {
+			glog.Errorln("An error occured while trying to Build the line from the []byte", err)
+		}
+		if strings.EqualFold(l.Content, l.BotNick+": ping") {
+			return &PluginResponse{
+				Action: RESPOND,
+				Body:   []byte("WRITE " + strconv.Itoa(l.ChatBotId) + " " + l.Channel + " Are you in need of my services, " + l.User + " ?"),
+			}
+		}
+	}
+	return &PluginResponse{}
+}
+
+// Debug is a debug plugin
+func Debug(b []byte) *PluginResponse {
+	glog.V(2).Infoln("Debug plugin", string(b))
+	return &PluginResponse{Action: NOOP, Body: b}
+}
+
+// DoPluginActions listen on the out chan for PluginResponse and perform this action
+func DoPluginActions(out chan *PluginResponse, ctx *Context, quit chan struct{}, wg sync.WaitGroup) {
+	wg.Add(1)
+	for {
+		select {
+		case <-quit:
+			wg.Done()
+		case pr := <-out:
+			err := pr.Action.Do(pr.Body, ctx)
+			if err != nil {
+				glog.Errorln("An error occured while performing this action", pr, err)
+			}
+		}
+	}
 }
 
 func main() {
@@ -83,30 +202,37 @@ func main() {
 	// Channels declaration
 	sigs := make(chan os.Signal, 1)
 	quit := make(chan struct{})
+	out := make(chan *PluginResponse)
 
 	// listen for syscall to cleanly terminate goroutine
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
-	plugins := []func(line.Line, *Context){EsStore, Ping, Debug}
+	pluginInChan := []chan []byte{
+		NewPlugin(EsStore).Run(out, quit, wg),
+		NewPlugin(Ping).Run(out, quit, wg),
+		NewPlugin(Debug).Run(out, quit, wg)}
+
 	context := &Context{
 		Storage: NewElasticsearch(esAddr, esIndex, esType),
 		Queue:   common.NewRedisQueue(),
 	}
 
+	go DoPluginActions(out, context, quit, wg)
+
 	lines := listen(wg, context, quit)
-	condition := true
-	for condition {
+	conditionFlag := true
+	for conditionFlag {
 		select {
-		case l := <-lines:
-			for _, p := range plugins {
-				p(l, context)
-			}
 		case <-sigs:
-			glog.Infoln("Closting botbot-elasticsearch")
 			close(quit)
+			conditionFlag = false
+			glog.Infoln("Closing the plugin manager")
 			wg.Wait()
-			condition = false
-			break
+		case l := <-lines:
+			glog.V(3).Infoln("Line", string(l))
+			for _, pic := range pluginInChan {
+				pic <- l
+			}
 		}
 	}
 }
